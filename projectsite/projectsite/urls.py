cec02d493d73b4c4565620a38d88d22b56e23f5b
Marvin Joseph <147320510+ImPresence2001@users.noreply.github.com>
2024-10-30 10:09:31 +0800

    Fix logout

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -44,5 +44,5 @@ urlpatterns = [
     path('program_list/<pk>', ProgramUpdateView.as_view(), name='program-update'),
     path('program_list/<pk>/delete', ProgramDeleteView.as_view(), name='program-delete'),
     re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
